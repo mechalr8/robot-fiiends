42a1a3793d2313b7f44ba75bd9e394652e9b83c4
mechalr8 <harsitagrawal59@gmail.com>
2022-09-24 04:24:17 +0530

    used the useEffect hook to update the array value

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,13 +1,8 @@
 import React from "react";
 import Card from "./Card";
-import { robots } from "../robots";
 
-const CardList = ({ input }) => {
-  const searchedRobots = robots.filter((robot) => {
-    if (input === "") return robot;
-    else return robot.name.toLowerCase().includes(input);
-  });
-  return searchedRobots.map((robot) => {
+const CardList = ({ robots }) => {
+  return robots.map((robot) => {
     return (
       <Card
         key={robot.id}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import "./App.css";
 const App = () => {
-  const [inputText, setInputText] = React.useState("");
+  const [inputText, setInputText] = useState("");
+  const [robots, setRobots] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setRobots(users);
+      });
+  }, []);
 
   const onSearchChange = (e) => {
-    setInputText(e.target.value.toLowerCase());
+    setInputText(e.target.value);
   };
+
+  const searchedRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(inputText.toLowerCase());
+  });
 
   return (
     <div className='tc'>
@@ -17,7 +30,7 @@ const App = () => {
         <SearchBox searchChange={onSearchChange} inputValue={inputText} />
       </div>
       <Scroll>
-        <CardList input={inputText} />
+        <CardList robots={searchedRobots} />
       </Scroll>
     </div>
   );
